92e07bdd7c3b1982dd3e5645d1433b493d9fb660
Kevin Wilder <kevin.wilder@darkwolfsolutions.com>
2022-08-09 20:01:45 -0600

    remove gradle lombok plugin

--- src/main/java/dod/p1/keycloak/common/YAMLConfig.java
+++ src/main/java/dod/p1/keycloak/common/YAMLConfig.java
@@ -2,39 +2,98 @@ package dod.p1.keycloak.common;
 
 import java.util.List;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
 public class YAMLConfig {
 
     /**
      * Yaml config x509.
      */
-    @Getter
-    @Setter
     private YAMLConfigX509 x509;
 
     /**
      * List of strings for group protection ignore clients.
      */
-    @Getter
-    @Setter
     private List<String> groupProtectionIgnoreClients;
 
     /**
      * List of strings for no email match auto join group.
      */
-    @Getter
-    @Setter
     private List<String> noEmailMatchAutoJoinGroup;
 
     /**
      * List of YAMLConfigEmailAutoJoin objects.
      */
-    @Getter
-    @Setter
     private List<YAMLConfigEmailAutoJoin> emailMatchAutoJoinGroup;
 
+    /**
+     * Constructor.
+     * @param pX509
+     */
+    public YAMLConfig(final YAMLConfigX509 pX509) {
+        this.x509 = pX509;
+    }
+
+    /**
+     * Get yaml config x509.
+     * @return YAMLConfigX509
+     */
+    public YAMLConfigX509 getX509() {
+        return x509;
+    }
+
+    /**
+     * Set x509.
+     * @param pX509
+     */
+    public void setX509(final YAMLConfigX509 pX509) {
+        this.x509 = x509;
+    }
+
+    /**
+     * Get Group Protection Ignore Clients.
+     * @return List
+     */
+    public List<String> getGroupProtectionIgnoreClients() {
+        return groupProtectionIgnoreClients;
+    }
+
+    /**
+     * Set GroupProtectionIgnoreClients.
+     * @param pGroupProtectionIgnoreClients
+     */
+    public void setGroupProtectionIgnoreClients(final List<String> pGroupProtectionIgnoreClients) {
+        this.groupProtectionIgnoreClients = groupProtectionIgnoreClients;
+    }
+
+    /**
+     * Get No Email Match Auto Join Group.
+     * @return List
+     */
+    public List<String> getNoEmailMatchAutoJoinGroup() {
+        return noEmailMatchAutoJoinGroup;
+    }
+
+    /**
+     * Set No Email Match Auto Join Group.
+     * @param pNoEmailMatchAutoJoinGroup
+     */
+    public void setNoEmailMatchAutoJoinGroup(final List<String> pNoEmailMatchAutoJoinGroup) {
+        this.noEmailMatchAutoJoinGroup = noEmailMatchAutoJoinGroup;
+    }
+
+    /**
+     * Get Email Match Auto Join Group.
+     * @return List
+     */
+    public List<YAMLConfigEmailAutoJoin> getEmailMatchAutoJoinGroup() {
+        return emailMatchAutoJoinGroup;
+    }
+
+    /**
+     * Set Email Match Auto Join Group.
+     * @param pEmailMatchAutoJoinGroup
+     */
+    public void setEmailMatchAutoJoinGroup(final List<YAMLConfigEmailAutoJoin> pEmailMatchAutoJoinGroup) {
+        this.emailMatchAutoJoinGroup = emailMatchAutoJoinGroup;
+    }
 }
--- src/main/java/dod/p1/keycloak/utils/NewObjectProvider.java
+++ src/main/java/dod/p1/keycloak/utils/NewObjectProvider.java
@@ -13,35 +13,35 @@ import java.io.FileNotFoundException;
  * The Jaccoco test coverage report is not fully compatible with the PowerMock testing framework
  * This class is used in @PrepareForTest so that the Jaccoco report detects test coverage for CommonConfig class.
  */
-public class NewObjectProvider {
+public final class NewObjectProvider {
 
     private NewObjectProvider() {
         // hide public constructor. No need to ever declare an instance. All methods are static.
     }
 
     /**
-     * Get new java.io.File object
+     * Get new java.io.File object.
      * @param filePath a String
      * @return File
      */
-    public static File getFile(String filePath){
+    public static File getFile(final String filePath) {
         return new File(filePath);
     }
 
     /**
-     * Get new java.io.FileInputStream object
+     * Get new java.io.FileInputStream object.
      * @param file a File object
      * @return FileInputStream
      */
-    public static FileInputStream getFileInputStream(File file) throws FileNotFoundException {
+    public static FileInputStream getFileInputStream(final File file) throws FileNotFoundException {
         return new FileInputStream(file);
     }
 
     /**
-     * Get new org.yaml.snakeyaml.Yaml object
+     * Get new org.yaml.snakeyaml.Yaml object.
      * @return Yaml
      */
-    public static Yaml getYaml(){
+    public static Yaml getYaml() {
       return new Yaml(new Constructor(YAMLConfig.class));
     }
 
